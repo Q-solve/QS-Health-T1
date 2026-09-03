443f0b1d06b800a08193d9b5e7b2d623d2ef1c84
Dativa Pereus <41068845+dativapereus@users.noreply.github.com>
2026-09-03 09:25:18 +0300

    Delete code/code/receptor directory
    delete all the files

--- code/code/receptor/prepare_receptor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Prepare the Chain A PfDHFR-TS receptor for molecular docking
-# Input: 6A2L_chainA.pdbqt
-# Tool: Meeko mk_prepare_receptor.py
-
-mk_prepare_receptor.py -i 6A2L_chainA.pdbqt
